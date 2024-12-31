daae924cf630ccac863aad526e5b6de0e8117ecf
aliosmantasin <ali.osman-07@hotmail.com>
2024-12-31 20:23:13 +0300

    upgrade mobil menu

--- app/(routes)/_components/Menu/MobilMenu.tsx
+++ app/(routes)/_components/Menu/MobilMenu.tsx
@@ -1,3 +1,4 @@
+"use client"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -5,7 +6,6 @@ import {
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet"
-
 import * as React from "react"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
@@ -18,14 +18,9 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
-
 import { ModeToggle } from "@/components/ModeToggle"
-import {menuLinksData, serviceLinkData} from "../Menu/Data/data";
+import { menuLinksData, serviceLinkData } from "../Menu/Data/data";
 import Image from "next/image"
-
-
-
-
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
@@ -51,102 +46,116 @@ const ListItem = React.forwardRef<
   )
 })
 ListItem.displayName = "ListItem"
-
-
-
 export function MobilMenu() {
-  
-
-  
-
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
   return (
     <div className="block sm:hidden space-y-5">
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="outline">Menü</Button>
-      </SheetTrigger>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger asChild>
+          <Button variant="outline">Menü</Button>
+        </SheetTrigger>
+        <SheetContent className="w-full p-2 h-full snap-y overflow-y-auto max-h-[100vh]">
+          <div className="flex space-x-5 mt-5 border-b-2 p-2 border-black-500">
+            <div><ModeToggle /></div>
+            <Button>Marka Kimliği</Button>
+          </div>
+          <NavigationMenu className="flex justify-start w-full max-w-96">
+            <NavigationMenuList className="flex flex-col space-y-5 mt-8 w-full">
+              <NavigationMenuItem className="flex w-full max-w-96">
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={cn(navigationMenuTriggerStyle(), "bg-transparent hover:bg-transparent text-[1rem]")}
+                    onClick={handleClose}
+                  >
+                    Ana Sayfa
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className="flex w-full max-w-96">
+                <NavigationMenuTrigger className={cn(navigationMenuTriggerStyle(), "bg-transparent hover:bg-transparent text-[1rem]")}>
+                  Hizmetler
+                </NavigationMenuTrigger>
+                <NavigationMenuContent className="">
+                  <ul className="grid gap-3 p-4">
+                    <li className="row-span-3">
+                      <Link
+                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        href="/"
+                        onClick={handleClose}
+                      >
+                        <div className="mx-auto">
+                          <Image
+                            src="/DigitalDonusumKutusu.webp"
+                            alt="Dijital Dönüşüm Kutusu"
+                            width={200}
+                            height={200}
+                          />
+                        </div>
+                        <div className="mb-2 mt-4 text-lg font-medium text-center">
+                          {serviceLinkData.convertionBox.map((item) => (
+                            <ListItem
+                              key={item.title}
+                              title={item.title}
+                              href={item.href}
+                              onClick={handleClose}
+                            >
+                              <p className="text-sm leading-tight text-muted-foreground text-center">
+                                {item.description}
+                              </p>
+                            </ListItem>
+                          ))}
+                        </div>
+                      </Link>
+                    </li>
+                    {serviceLinkData.singleService.map((item) => (
+                      <ListItem
+                        key={item.title}
+                        href={item.href}
+                        title={item.title}
+                        className="row-span-4"
+                        onClick={handleClose}
+                      >
+                        <p>{item.description}</p>
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
 
-      <SheetContent className="w-full p-2 h-full snap-y overflow-y-auto max-h-[100vh]">
-  <div className="flex space-x-5 mt-5 border-b-2 p-2 border-black-500 ">
-    <div className=""><ModeToggle /></div>
-    <Button className="">Marka Kimliği</Button>
-  </div>
-       
-  <NavigationMenu className="flex justify-start w-full max-w-96">
-    <NavigationMenuList className="flex flex-col space-y-5 mt-8 w-full">
-      
-      <NavigationMenuItem className="flex w-full max-w-96">
-        <Link href="/" legacyBehavior passHref>
-          <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "bg-transparent hover:bg-transparent text-[1rem]")}>
-            Ana Sayfa
-          </NavigationMenuLink>
-        </Link>
-      </NavigationMenuItem>
-
-      <NavigationMenuItem className="flex w-full max-w-96">
-        <NavigationMenuTrigger className={cn(navigationMenuTriggerStyle(), "bg-transparent hover:bg-transparent text-[1rem]")}>Hizmetler</NavigationMenuTrigger>
-        <NavigationMenuContent className="">
-          <ul className="grid gap-3 p-4">
-            <li className="row-span-3">
-              <Link
-                className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                href="/"
-              >
-                <div className="mx-auto">
-                  <Image
-                    src="/DigitalDonusumKutusu.webp"
-                    alt="Dijital Dönüşüm Kutusu"
-                    width={200}
-                    height={200}
-                  />
-                </div>
-                <div className="mb-2 mt-4 text-lg font-medium text-center">
-                  {serviceLinkData.convertionBox.map((item) => (
-                    <ListItem key={item.title} title={item.title} href={item.href}>
-                      <p className="text-sm leading-tight text-muted-foreground text-center">
+              <NavigationMenuItem className="flex w-full max-w-96">
+                <NavigationMenuTrigger className={cn(navigationMenuTriggerStyle(), "bg-transparent hover:bg-transparent text-[1rem]")}>
+                  Sektörler
+                </NavigationMenuTrigger>
+                <NavigationMenuContent className="overflow-y-auto max-h-[60vh]">
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                    {menuLinksData.map((item) => (
+                      <ListItem
+                        key={item.title}
+                        title={item.title}
+                        href={item.href}
+                        onClick={handleClose}
+                      >
                         {item.description}
-                      </p>
-                    </ListItem>
-                  ))}
-                </div>
-              </Link>
-            </li>
-            {serviceLinkData.singleService.map((item) => (
-              <ListItem key={item.title} href={item.href} title={item.title} className="row-span-4">
-                <p>{item.description}</p>
-              </ListItem>
-            ))}
-          </ul>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-
-      <NavigationMenuItem className="flex w-full max-w-96">
-        <NavigationMenuTrigger className={cn(navigationMenuTriggerStyle(), "bg-transparent hover:bg-transparent text-[1rem]")}>Sektörler</NavigationMenuTrigger>
-        <NavigationMenuContent className="overflow-y-auto max-h-[60vh]">
-          <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-            {menuLinksData.map((item) => (
-              <ListItem key={item.title} title={item.title} href={item.href}>
-                {item.description}
-              </ListItem>
-            ))}
-          </ul>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-
-      <NavigationMenuItem className="flex w-full max-w-96">
-        <Link href="/referans" legacyBehavior passHref>
-          <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "bg-transparent hover:bg-transparent text-[1rem]")}>
-            Referanslar
-          </NavigationMenuLink>
-        </Link>
-      </NavigationMenuItem>
-           
-    </NavigationMenuList>
-  </NavigationMenu>
-</SheetContent>
-
-      
-    </Sheet>
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem className="flex w-full max-w-96">
+                <Link href="/referans" legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={cn(navigationMenuTriggerStyle(), "bg-transparent hover:bg-transparent text-[1rem]")}
+                    onClick={handleClose}
+                  >
+                    Referanslar
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </SheetContent>
+      </Sheet>
     </div>
   )
 }
