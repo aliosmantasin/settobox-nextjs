f09c87089f09a3cb2a669408527ba8208e40645a
aliosmantasin <ali.osman-07@hotmail.com>
2024-12-31 19:52:43 +0300

    upgrade DijitalDonusumKutusu import with next/dynamic

--- app/(routes)/(anasayfa)/page.tsx
+++ app/(routes)/(anasayfa)/page.tsx
@@ -1,10 +1,9 @@
 'use client'
-
-import DijitalDonusumKutusu from "../_components/HomePage/DijitalDonusumKutusu/DijitalDonusumKutusu";
+import dynamic from "next/dynamic";
 import FirstGlance from "../_components/HomePage/FirstGlance";
 import {MaskSvg} from "../_components/HomePage/Mask/Mask";
 import ServiceSlider from "../_components/HomePage/SwiperSlider/SwiperSlider";
-
+const DijitalDonusumKutusu = dynamic(() => import("../_components/HomePage/DijitalDonusumKutusu/DijitalDonusumKutusu"), { ssr: false });
 
 
 
