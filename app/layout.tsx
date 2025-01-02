f28cf6f91319a50698952564220937e2e13c3c89
aliosmantasin <ali.osman-07@hotmail.com>
2025-01-03 01:27:56 +0300

    fixed GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
-      <GoogleTagManager gtmId="GTM-XYZ" />
+      <GoogleTagManager gtmId="GTM-NRSTMB28" />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
